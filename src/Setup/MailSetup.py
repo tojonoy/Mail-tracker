45a87abd9f74e4e31aa294ba080ab6dd7fab0dd8
tojonoy <thomasjohn3933@gmail.com>
2025-06-04 23:28:57 +0530

    final commit v1

--- src/Setup/MailSetup.py
+++ src/Setup/MailSetup.py
@@ -2,18 +2,19 @@ import os
 import ezgmail
 import os
 import json
+import shutil
 
 def initialize_gmail():
 
     creds_content = os.getenv("CREDENTIALS_JSON")
+    token_path = "/etc/secrets/token.json"
     with open("credentials.json", "w") as f:
         f.write(creds_content)
-    if not os.path.exists('token.json'):
-        print("Running ezgmail.init() for the first time...")
+    shutil.copy(token_path,"token.json")
+    if not ezgmail.isAuthenticated():
         ezgmail.init(
     credentialsFile='credentials.json',
     tokenFile='token.json',
-)
-
+    )
     else:
         print("Gmail already initialized.")
