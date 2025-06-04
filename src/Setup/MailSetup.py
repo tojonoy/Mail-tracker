1abae833be4523ba769711d80c96f8529c4fd5a9
tojonoy <thomasjohn3933@gmail.com>
2025-06-05 00:00:03 +0530

    final

--- src/Setup/MailSetup.py
+++ src/Setup/MailSetup.py
@@ -1,15 +1,10 @@
 import os
 import ezgmail
-import os
-import json
-import shutil
 
 def initialize_gmail():
-    shutil.copy('/etc/secrets/credentials.json', 'credentials.json')
-    ezgmail.init(credentialsFile='credentials.json')  # Run locally
-    return
-    shutil.copy('/etc/secrets/credentials.json', 'credentials.json')
-    shutil.copy('/etc/secrets/token.json', 'token.json')
-    print("Credentials and token files copied successfully.")
-    ezgmail.init(credentialsFile='credentials.json', tokenFile='token.json')
-    print("Gmail initialized successfully.")
+    if os.getenv("RUNNING_ON_RENDER"):
+        token_file = '/opt/render/project/src/token.pickle'  # Use the pickle path
+    else:
+        token_file = 'token.json'
+    
+    ezgmail.init(credentialsFile='credentials.json', tokenFile=token_file)
