db4cb846d2711de2f9d4168315ca0f46258b3d5a
tojonoy <thomasjohn3933@gmail.com>
2025-06-04 23:52:08 +0530

    final commit v5

--- src/Setup/MailSetup.py
+++ src/Setup/MailSetup.py
@@ -6,6 +6,9 @@ import shutil
 
 def initialize_gmail():
     shutil.copy('/etc/secrets/credentials.json', 'credentials.json')
+    ezgmail.init(credentialsFile='credentials.json')  # Run locally
+    return
+    shutil.copy('/etc/secrets/credentials.json', 'credentials.json')
     shutil.copy('/etc/secrets/token.json', 'token.json')
     print("Credentials and token files copied successfully.")
     ezgmail.init(credentialsFile='credentials.json', tokenFile='token.json')
