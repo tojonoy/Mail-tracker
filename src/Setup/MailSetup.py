c6780f061d9f9f03817f488828ce93870cf104f0
tojonoy <thomasjohn3933@gmail.com>
2025-06-05 00:01:36 +0530

    fin

--- src/Setup/MailSetup.py
+++ src/Setup/MailSetup.py
@@ -3,8 +3,10 @@ import ezgmail
 
 def initialize_gmail():
     if os.getenv("RUNNING_ON_RENDER"):
+        credentials_file = '/opt/render/project/src/credentials.json'
         token_file = '/opt/render/project/src/token.pickle'  # Use the pickle path
     else:
+        credentials_file = 'credentials.json'
         token_file = 'token.json'
     
-    ezgmail.init(credentialsFile='credentials.json', tokenFile=token_file)
+    ezgmail.init(credentialsFile=credentials_file, tokenFile=token_file)
